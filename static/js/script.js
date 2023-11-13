63bc1a12f93043cb578645c763acf07dae733440
halimaprecious <halimaprecious3@gmail.com>
2023-11-13 16:28:28 +0300

    WIP:Blackjack functionality

--- static/js/script.js
+++ static/js/script.js
@@ -164,3 +164,43 @@ function randomColors(){
         allButtons[i].classList.add(choices[randomNumber]);
     }
 }
+
+// Challenge 5: Blackjack
+let blackjackGame = {
+    'you': {'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0},
+    'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0},
+};
+
+const YOU = blackjackGame['you']
+const DEALER = blackjackGame['dealer']
+
+const hitSound = new Audio('static/sounds/swish.m4a');
+
+document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal)
+
+
+function blackjackHit(){
+    showCard(YOU);
+}
+
+function showCard(activePlayer){
+    let cardImage = document.createElement('img');
+    cardImage.src = 'static/images/Q.png';
+    document.querySelector(activePlayer['div']).appendChild(cardImage);
+    hitSound.play();
+}
+
+function blackjackDeal(){
+    let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+    let deakrImages = document.querySelector('#dealer-box').querySelectorAll('img');
+   
+    for(i=0; i < yourImages.length; i++){
+        yourImages.remove();
+    }
+
+    for(i=0; i < dealerImages.length; i++){
+        dealerImages.remove();
+    }
+}
