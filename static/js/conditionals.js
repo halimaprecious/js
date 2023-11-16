4f2207db773f3c026ad0d615ace1ee52ad5361a5
halimaprecious <halimaprecious3@gmail.com>
2023-11-16 22:20:22 +0300

    js conditionals practice

--- static/js/conditionals.js
+++ static/js/conditionals.js
@@ -1,24 +1,28 @@
 // day 4 exercise
 // 1. Age checker
 
-document.querySelector('#ageForm').addEventListener('submit', function (event) {
-    event.preventDefault();
+document.querySelector("#ageForm").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    // Get user input
-    var age = document.getElementById("age").value;
+  // Get user input
+  var age = document.getElementById("age").value;
 
-    // Check if the input is a valid number
-    if (isNaN(age)) {
-        document.getElementById("results").innerText = "Please enter a valid age.";
+  // Check if the input is a valid number
+  if (isNaN(age)) {
+    document.getElementById("results").innerText = "Please enter a valid age.";
+  } else {
+    // Check if the user is 18 or older
+    if (age >= 18) {
+      document.getElementById("results").innerText =
+        "You are old enough to drive.";
     } else {
-        // Check if the user is 18 or older
-        if (age >= 18) {
-            document.getElementById("results").innerText = "You are old enough to drive.";
-        } else {
-            var yearsToWait = 18 - age;
-            document.getElementById("results").innerText = "Sorry, you are not old enough to drive.\n Please wait for " + yearsToWait + " more year(s).";
-        }
+      var yearsToWait = 18 - age;
+      document.getElementById("results").innerText =
+        "Sorry, you are not old enough to drive.\n Please wait for " +
+        yearsToWait +
+        " more year(s).";
     }
+  }
 });
 
 // ternary operator
@@ -26,25 +30,58 @@ document.querySelector('#ageForm').addEventListener('submit', function (event) {
 let a = 4;
 let b = 3;
 
-if (a > b){
-    console.log( `${a} is greater than ${b}`)
-}else{
-    console.log(`${a} is less than ${b}`)
+if (a > b) {
+  console.log(`${a} is greater than ${b}`);
+} else {
+  console.log(`${a} is less than ${b}`);
 }
 
 // even numbers
-document.querySelector('#myForm').addEventListener('submit',function(event){
-    event.preventDefault();
+document.querySelector("#myForm").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-let number = document.getElementById('even-num').value;
+  let number = document.getElementById("even-num").value;
 
-if(isNaN(number)){
-    document.getElementById('output').innerHTML = 'Please enter a valid number';
-}else{
-    if(number % 2 === 0){
-        document.getElementById('output').innerHTML = `${number} is an even number `;   
-    }else{
-        document.getElementById('output').innerHTML = `${number} is an odd number `;  
+  if (isNaN(number)) {
+    document.getElementById("output").innerHTML = "Please enter a valid number";
+  } else {
+    if (number % 2 === 0) {
+      document.getElementById(
+        "output"
+      ).innerHTML = `${number} is an even number `;
+    } else {
+      document.getElementById(
+        "output"
+      ).innerHTML = `${number} is an odd number `;
     }
-}
+  }
 });
+
+// Grading scheme
+document.querySelector("#grades").addEventListener("click", checkGrade);
+function checkGrade() {
+  // user input(score)
+  let score = document.getElementById("score").value;
+
+  if ((score === '')) {
+    document.getElementById("gradeResults").innerHTML = `<p style='color:red;'> You must enter a valid score!! </p>`;
+  } else {
+    if (score >= 80 && score <= 100) {
+    document.getElementById("gradeResults").innerHTML = "Your score is: A";
+
+    } else if (score >= 70  && score < 80) {
+        document.getElementById("gradeResults").innerHTML = "Your score is: B";
+    } else if (score >= 60 && score < 70) {
+        document.getElementById("gradeResults").innerHTML = "Your score is: C";
+
+    } else if (score >= 50 && score < 60) {
+        document.getElementById("gradeResults").innerHTML = "Your score is: D";
+
+    } else if (score >= 0 && score < 50) {
+        document.getElementById("gradeResults").innerHTML = "Your score is: F";
+
+    }else{
+        console.log('Invalid score. Score ,must be between 0 and 100')
+    }
+  }
+}
